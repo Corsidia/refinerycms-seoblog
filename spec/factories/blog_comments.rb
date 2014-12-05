646ddbf11ad36f2e240d809c74fb20219ea47adc
duccioarmenise <duccioarmenise@gmail.com>
2014-12-05 10:33:47 +0100

    trim white space

--- spec/factories/blog_comments.rb
+++ spec/factories/blog_comments.rb
@@ -4,15 +4,15 @@ FactoryGirl.define do
     sequence(:email) { |n| "person#{n}@example.com" }
     body "Which one is the best for picking up new shoes?"
     association :post, :factory => :blog_post
-  
+
     trait :approved do
       state 'approved'
     end
-  
+
     trait :rejected do
       state 'rejected'
     end
-    
+
     factory :approved_comment, :traits => [:approved]
     factory :rejected_comment, :traits => [:rejected]
   end
