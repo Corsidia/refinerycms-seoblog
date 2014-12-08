07878276e80a192a21ac38138323d4d8630d186c
duccioarmenise <duccioarmenise@gmail.com>
2014-12-08 18:29:40 +0100

    removed Admin Settings for Comment notification and moderation

--- app/controllers/refinery/blog/admin/settings_controller.rb
+++ app/controllers/refinery/blog/admin/settings_controller.rb
@@ -3,32 +3,6 @@ module Refinery
     module Admin
       class SettingsController < ::Refinery::AdminController
 
-        def notification_recipients
-          @recipients = Refinery::Blog::Comment::Notification.recipients
-
-          if request.post?
-            Refinery::Blog::Comment::Notification.recipients = params[:recipients]
-            flash[:notice] = t('updated', :scope => 'refinery.blog.admin.settings.notification_recipients',
-                               :recipients => Refinery::Blog::Comment::Notification.recipients)
-            unless request.xhr? or from_dialog?
-              redirect_back_or_default(refinery.blog_admin_posts_path)
-            else
-              render :text => "<script type='text/javascript'>parent.window.location = '#{refinery.blog_admin_posts_path}';</script>",
-                     :layout => false
-            end
-          end
-        end
-
-        def moderation
-          enabled = Refinery::Blog::Comment::Moderation.toggle!
-          unless request.xhr?
-            redirect_back_or_default(refinery.blog_admin_posts_path)
-          else
-            render :json => {:enabled => enabled},
-                   :layout => false
-          end
-        end
-
         def teasers
           enabled = Refinery::Blog::Post.teaser_enabled_toggle!
           unless request.xhr?
